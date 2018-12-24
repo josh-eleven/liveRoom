851f61c27f3cb5adf24ab89b88be4d8e538214a6
yansong <yansong@douyu.tv>
2018-12-24 09:48:24 +0800

    应用eslint

--- src/models/myVideo.js
+++ src/models/myVideo.js
@@ -1,7 +1,7 @@
 export default {
-    namespace: "streamUrl", // 表示在全局 state 上的 key
-    state: '', // 是初始值，在这里是空数组
-    reducers: { // 等同于 redux 里的 reducer，接收 action，同步更新 state
+	namespace: 'streamUrl', // 表示在全局 state 上的 key
+	state: '', // 是初始值，在这里是空数组
+	reducers: { // 等同于 redux 里的 reducer，接收 action，同步更新 state
        
-    }
+	}
 };
